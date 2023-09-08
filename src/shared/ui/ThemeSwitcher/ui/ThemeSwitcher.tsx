3b224238f5852ee8eb7d7a3f5de52b5b5bfdacff
ivanova-an <the_ivangelina@outlook.com>
2023-09-08 04:01:11 -0700

    add stylelint

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,24 +1,22 @@
-import {classNames} from "shared/lib/classNames/classNames";
-import cls from './ThemeSwitcher.module.scss'
-import React from "react";
-import {Theme, useTheme} from "app/providers/ThemeProvider";
+import { classNames } from 'shared/lib/classNames/classNames';
+import React from 'react';
+import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
-import {Button, ThemeButton} from "shared/ui/Button/ui/Button";
+import { Button, ThemeButton } from 'shared/ui/Button/ui/Button';
 
 interface ThemeSwitcherProps {
     className? : string;
 }
-export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
+export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
     const { theme, toggleTheme } = useTheme();
     return (
         <Button
             theme={ThemeButton.CLEAR}
-            className={classNames(cls.ThemeSwitcher, {}, [className])}
+            className={classNames('', {}, [className])}
             onClick={toggleTheme}
         >
-            {theme === Theme.DARK? <DarkIcon/> : <LightIcon/>}
+            {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
         </Button>
     );
 };
-
