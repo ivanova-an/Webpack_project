342852cc574ec9654792eebc7ae16b387fceb01e
ivanova-an <the_ivangelina@outlook.com>
2023-09-17 09:05:31 -0700

    add reducerManager

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,19 +1,32 @@
-import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
+import { configureStore, DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { counterReducer } from 'entities/Counter';
 import { userReducer } from 'entities/User';
-import { loginReducer } from 'features/AuthByUsername';
+import { createReducerManager } from 'app/providers/StoreProvider/config/reduserManager';
 import { StateSchema } from './StateSchema';
 
-export function createReduxStore(initialState?: StateSchema) {
+export function createReduxStore(
+    initialState?: StateSchema,
+    asyncReducers?: ReducersMapObject<StateSchema>,
+) {
+    // Оставляем те редючеры которые являются обязательными
     const rootReducers: ReducersMapObject<StateSchema> = {
+        ...asyncReducers,
         counter: counterReducer,
         user: userReducer,
-        loginForm: loginReducer,
     };
+    // Создадим reducerManager   в createReducerManager(список корневых редюсеров)
+    const reducerManager = createReducerManager(rootReducers);
 
-    return configureStore<StateSchema>({
-        reducer: rootReducers,
+    const store = configureStore<StateSchema>({
+        // добавляем наш reducerManager
+        reducer: reducerManager.reduce,
         devTools: __IS_DEV__,
         preloadedState: initialState,
     });
+
+    // к самому Store reducerManager нужно добавить
+    // @ts-ignore
+    store.reducerManager = reducerManager;
+
+    return store;
 }
